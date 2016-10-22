f40e2f14477be571ff7d1697420d980c04dac806
Yuting Liu <yuting9@gmail.com>
2016-10-22 12:43:07 -0400

    Changes

--- src/page.java
+++ src/page.java
@@ -4,10 +4,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
 
 public class page {
 	private static JTextField txtName;
@@ -66,13 +62,21 @@ public class page {
 		lblName.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		panel_2.add(lblName);
 		
+		JComboBox classCombo = new JComboBox();
+		sl_panel_2.putConstraint(SpringLayout.EAST, classCombo, 0, SpringLayout.EAST, txtName);
+		classCombo.setToolTipText("Choose a race");
+		sl_panel_2.putConstraint(SpringLayout.NORTH, classCombo, 6, SpringLayout.SOUTH, txtName);
+		sl_panel_2.putConstraint(SpringLayout.WEST, classCombo, 10, SpringLayout.WEST, panel_2);
+		classCombo.setModel(new DefaultComboBoxModel(new String[] {"   ","Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"}));
+		panel_2.add(classCombo);
+		
 		JLabel lblAlignment = new JLabel("Choose Alignment");
 		lblAlignment.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		JComboBox combo1 = new JComboBox();
-		sl_panel_2.putConstraint(SpringLayout.WEST, combo1, 6, SpringLayout.EAST, lblName);
 		combo1.setModel(new DefaultComboBoxModel(new String[] {"--Ideals","Lawful", "Neutral", "Chaotic"}));
 		sl_panel_2.putConstraint(SpringLayout.NORTH, combo1, 0, SpringLayout.NORTH, txtName);
+		sl_panel_2.putConstraint(SpringLayout.WEST, combo1, 6, SpringLayout.EAST, lblName);
 		panel_2.add(combo1);
 		
 		JComboBox combo2 = new JComboBox();
@@ -114,7 +118,38 @@ public class page {
 		panel_2.add(lblAlignment);
 		
 		
+		JLabel lblClass = new JLabel("Class");
+		sl_panel_2.putConstraint(SpringLayout.NORTH, lblClass, -1, SpringLayout.NORTH, classCombo);
+		sl_panel_2.putConstraint(SpringLayout.WEST, lblClass, 0, SpringLayout.WEST, lblName);
+		lblClass.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel_2.add(lblClass);
 		
+		DefaultComboBoxModel def = new DefaultComboBoxModel(new String[] {"N/A"});
+		DefaultComboBoxModel dwr = new DefaultComboBoxModel(new String[] {"Hill Dwarf", "Mountain Dwarf"});
+		DefaultComboBoxModel hal = new DefaultComboBoxModel(new String[] {"Ligntfoot", "Stout"});
+		DefaultComboBoxModel elf = new DefaultComboBoxModel(new String[] {"High Elf", "Wood Elf", "Dark Elf"});
+		DefaultComboBoxModel hum = new DefaultComboBoxModel(new String[] {"Calishite", "Chondathan",
+																		  "Damaran", "Illuskan", "Mulan", "Rashemi",
+																		  "Shou", "Tethyran", "Turami"});
+		DefaultComboBoxModel dgb = new DefaultComboBoxModel(new String[] {"Black", "Blue", "Brass", "Bronze", "Copper",
+																		  "Gold", "Green", "Red", "Silver", "White"});
+		DefaultComboBoxModel gnm = new DefaultComboBoxModel(new String[] {"Forest Gnome", "Rock Gnome", "Deep Gnome"});
+		DefaultComboBoxModel hlf = new DefaultComboBoxModel(new String[] {"N/A"});
+		DefaultComboBoxModel hor = new DefaultComboBoxModel(new String[] {"N/A"});
+		DefaultComboBoxModel tie = new DefaultComboBoxModel(new String[] {"N/A"});
+		
+		JComboBox subCombo = new JComboBox();
+		subCombo.setModel(def);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, subCombo, 6, SpringLayout.SOUTH, classCombo);
+		sl_panel_2.putConstraint(SpringLayout.WEST, subCombo, 0, SpringLayout.WEST, txtName);
+		panel_2.add(subCombo);
+		
+		JLabel lblSubclass = new JLabel("Subclass");
+		sl_panel_2.putConstraint(SpringLayout.EAST, subCombo, -6, SpringLayout.WEST, lblSubclass);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, lblSubclass, 6, SpringLayout.SOUTH, lblClass);
+		sl_panel_2.putConstraint(SpringLayout.WEST, lblSubclass, 0, SpringLayout.WEST, lblName);
+		lblSubclass.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel_2.add(lblSubclass);
 		
 		JTextArea textArea = new JTextArea();
 		textArea.setLineWrap(true);
@@ -137,119 +172,65 @@ public class page {
 		panel_2.add(lblCharacter);
 		
 		textField = new JTextField();
-		sl_panel_2.putConstraint(SpringLayout.WEST, textField, 0, SpringLayout.WEST, txtName);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, textField, 40, SpringLayout.SOUTH, subCombo);
+		sl_panel_2.putConstraint(SpringLayout.EAST, textField, 0, SpringLayout.EAST, txtName);
 		panel_2.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		sl_panel_2.putConstraint(SpringLayout.SOUTH, textField, -6, SpringLayout.NORTH, textField_1);
-		sl_panel_2.putConstraint(SpringLayout.WEST, textField_1, 0, SpringLayout.WEST, txtName);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, textField_1, 6, SpringLayout.SOUTH, textField);
+		sl_panel_2.putConstraint(SpringLayout.EAST, textField_1, 0, SpringLayout.EAST, txtName);
 		panel_2.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		sl_panel_2.putConstraint(SpringLayout.SOUTH, textField_1, -6, SpringLayout.NORTH, textField_2);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, textField_2, 6, SpringLayout.SOUTH, textField_1);
 		sl_panel_2.putConstraint(SpringLayout.WEST, textField_2, 0, SpringLayout.WEST, txtName);
 		panel_2.add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		sl_panel_2.putConstraint(SpringLayout.SOUTH, textField_2, -6, SpringLayout.NORTH, textField_3);
-		sl_panel_2.putConstraint(SpringLayout.EAST, textField_3, 0, SpringLayout.EAST, txtName);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, textField_3, 6, SpringLayout.SOUTH, textField_2);
+		sl_panel_2.putConstraint(SpringLayout.WEST, textField_3, 0, SpringLayout.WEST, txtName);
 		panel_2.add(textField_3);
 		textField_3.setColumns(10);
 		
 		JLabel lblEyeColor = new JLabel("Eye Color");
-		sl_panel_2.putConstraint(SpringLayout.NORTH, lblEyeColor, -1, SpringLayout.NORTH, textField);
 		sl_panel_2.putConstraint(SpringLayout.WEST, lblEyeColor, 0, SpringLayout.WEST, lblName);
+		sl_panel_2.putConstraint(SpringLayout.SOUTH, lblEyeColor, 0, SpringLayout.SOUTH, textField);
 		lblEyeColor.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		panel_2.add(lblEyeColor);
 		
 		JLabel lblHairColor = new JLabel("Hair Color");
-		sl_panel_2.putConstraint(SpringLayout.NORTH, lblHairColor, -1, SpringLayout.NORTH, textField_1);
-		sl_panel_2.putConstraint(SpringLayout.WEST, lblHairColor, 6, SpringLayout.EAST, textField_1);
+		sl_panel_2.putConstraint(SpringLayout.WEST, lblHairColor, 0, SpringLayout.WEST, lblName);
+		sl_panel_2.putConstraint(SpringLayout.SOUTH, lblHairColor, 0, SpringLayout.SOUTH, textField_1);
 		lblHairColor.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		panel_2.add(lblHairColor);
 		
 		JLabel lblSkinColor = new JLabel("Skin Color");
-		sl_panel_2.putConstraint(SpringLayout.NORTH, lblSkinColor, -1, SpringLayout.NORTH, textField_2);
-		sl_panel_2.putConstraint(SpringLayout.WEST, lblSkinColor, 5, SpringLayout.EAST, textField_2);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, lblSkinColor, -19, SpringLayout.SOUTH, textField_2);
+		sl_panel_2.putConstraint(SpringLayout.WEST, lblSkinColor, 6, SpringLayout.EAST, textField_2);
+		sl_panel_2.putConstraint(SpringLayout.SOUTH, lblSkinColor, 0, SpringLayout.SOUTH, textField_2);
 		lblSkinColor.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		panel_2.add(lblSkinColor);
 		
 		JLabel lblHeight = new JLabel("Height");
-		sl_panel_2.putConstraint(SpringLayout.WEST, lblHeight, 6, SpringLayout.EAST, textField_3);
-		sl_panel_2.putConstraint(SpringLayout.SOUTH, lblSkinColor, -7, SpringLayout.NORTH, lblHeight);
-		sl_panel_2.putConstraint(SpringLayout.NORTH, lblHeight, -1, SpringLayout.NORTH, textField_3);
+		sl_panel_2.putConstraint(SpringLayout.WEST, lblHeight, 0, SpringLayout.WEST, lblName);
+		sl_panel_2.putConstraint(SpringLayout.SOUTH, lblHeight, 0, SpringLayout.SOUTH, textField_3);
 		lblHeight.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		panel_2.add(lblHeight);
 		
 		JLabel lblWeight = new JLabel("Weight");
+		sl_panel_2.putConstraint(SpringLayout.NORTH, lblWeight, 6, SpringLayout.SOUTH, lblHeight);
+		sl_panel_2.putConstraint(SpringLayout.WEST, lblWeight, 0, SpringLayout.WEST, lblName);
 		lblWeight.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		panel_2.add(lblWeight);
 		
 		textField_4 = new JTextField();
-		sl_panel_2.putConstraint(SpringLayout.WEST, lblWeight, 7, SpringLayout.EAST, textField_4);
-		sl_panel_2.putConstraint(SpringLayout.SOUTH, textField_3, -6, SpringLayout.NORTH, textField_4);
-		sl_panel_2.putConstraint(SpringLayout.NORTH, lblWeight, -1, SpringLayout.NORTH, textField_4);
-		sl_panel_2.putConstraint(SpringLayout.SOUTH, textField_4, -12, SpringLayout.NORTH, scrollBar);
-		sl_panel_2.putConstraint(SpringLayout.EAST, textField_4, 0, SpringLayout.EAST, txtName);
+		sl_panel_2.putConstraint(SpringLayout.NORTH, textField_4, 6, SpringLayout.SOUTH, textField_3);
+		sl_panel_2.putConstraint(SpringLayout.WEST, textField_4, 0, SpringLayout.WEST, txtName);
 		textField_4.setColumns(10);
 		panel_2.add(textField_4);
-		
-		JPanel panel_3 = new JPanel();
-		tabbedPane.addTab("Class", null, panel_3, null);
-		SpringLayout springLayout = new SpringLayout();
-		panel_3.setLayout(springLayout);
-
-		JComboBox classCombo = new JComboBox();
-		springLayout.putConstraint(SpringLayout.NORTH, classCombo, 10, SpringLayout.NORTH, panel_3);
-		springLayout.putConstraint(SpringLayout.WEST, classCombo, 10, SpringLayout.WEST, panel_3);
-		sl_panel_2.putConstraint(SpringLayout.EAST, classCombo, 0, SpringLayout.EAST, txtName);
-		classCombo.setToolTipText("Choose a race");
-		sl_panel_2.putConstraint(SpringLayout.NORTH, classCombo, 6, SpringLayout.SOUTH, txtName);
-		sl_panel_2.putConstraint(SpringLayout.WEST, classCombo, 10, SpringLayout.WEST, panel_2);
-		classCombo.setModel(new DefaultComboBoxModel(new String[] {"   ","Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"}));
-		panel_3.add(classCombo);
-
-		JLabel lblSubclass = new JLabel("Subclass");
-		springLayout.putConstraint(SpringLayout.NORTH, lblSubclass, -1, SpringLayout.NORTH, classCombo);
-		lblSubclass.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		panel_3.add(lblSubclass);
-
-		JLabel lblClass = new JLabel("Class");
-		springLayout.putConstraint(SpringLayout.NORTH, lblClass, -1, SpringLayout.NORTH, classCombo);
-		springLayout.putConstraint(SpringLayout.WEST, lblClass, 6, SpringLayout.EAST, classCombo);
-		sl_panel_2.putConstraint(SpringLayout.NORTH, lblClass, -1, SpringLayout.NORTH, classCombo);
-		lblClass.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		panel_3.add(lblClass);
-		
-		
-		DefaultComboBoxModel def = new DefaultComboBoxModel(new String[] {"N/A"});
-		DefaultComboBoxModel dwr = new DefaultComboBoxModel(new String[] {"Hill Dwarf", "Mountain Dwarf"});
-		DefaultComboBoxModel hal = new DefaultComboBoxModel(new String[] {"Ligntfoot", "Stout"});
-		DefaultComboBoxModel elf = new DefaultComboBoxModel(new String[] {"High Elf", "Wood Elf", "Dark Elf"});
-		DefaultComboBoxModel hum = new DefaultComboBoxModel(new String[] {"Calishite", "Chondathan",
-																		  "Damaran", "Illuskan", "Mulan", "Rashemi",
-																		  "Shou", "Tethyran", "Turami"});
-		DefaultComboBoxModel dgb = new DefaultComboBoxModel(new String[] {"Black", "Blue", "Brass", "Bronze", "Copper",
-																		  "Gold", "Green", "Red", "Silver", "White"});
-		DefaultComboBoxModel gnm = new DefaultComboBoxModel(new String[] {"Forest Gnome", "Rock Gnome", "Deep Gnome"});
-		DefaultComboBoxModel hlf = new DefaultComboBoxModel(new String[] {"N/A"});
-		DefaultComboBoxModel hor = new DefaultComboBoxModel(new String[] {"N/A"});
-		DefaultComboBoxModel tie = new DefaultComboBoxModel(new String[] {"N/A"});
-
-		JComboBox subCombo = new JComboBox();
-		springLayout.putConstraint(SpringLayout.WEST, lblSubclass, 6, SpringLayout.EAST, subCombo);
-		springLayout.putConstraint(SpringLayout.EAST, subCombo, 170, SpringLayout.EAST, classCombo);
-		springLayout.putConstraint(SpringLayout.NORTH, subCombo, 0, SpringLayout.NORTH, classCombo);
-		springLayout.putConstraint(SpringLayout.WEST, subCombo, 10, SpringLayout.EAST, lblClass);
-		sl_panel_2.putConstraint(SpringLayout.NORTH, lblSubclass, -1, SpringLayout.NORTH, subCombo);
-		sl_panel_2.putConstraint(SpringLayout.WEST, lblSubclass, 55, SpringLayout.EAST, subCombo);
-		subCombo.setModel(def);
-		sl_panel_2.putConstraint(SpringLayout.NORTH, subCombo, 6, SpringLayout.SOUTH, classCombo);
-		sl_panel_2.putConstraint(SpringLayout.WEST, subCombo, 0, SpringLayout.WEST, txtName);
-		panel_3.add(subCombo);
 		
 		classCombo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -288,12 +269,23 @@ public class page {
 			}
 		});
 		
+		JPanel panel_3 = new JPanel();
+		tabbedPane.addTab("Attributes", null, panel_3, null);
+		panel_3.setLayout(null);
+		
+		int str, dex, con, intl, wis, chr, pts = 47;
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setBounds(10, 11, 31, 26);
+		panel_3.add(textPane);
+		
+		JSlider slider = new JSlider();
+		slider.setBounds(41, 11, 200, 26);
+		panel_3.add(slider);
 		
 		JPanel panel_4 = new JPanel();
-		tabbedPane.addTab("Attributes", null, panel_4, null);
-		
-		JPanel panel_5 = new JPanel();
-		tabbedPane.addTab("Details", null, panel_5, null);
+		tabbedPane.addTab("Details", null, panel_4, null);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
