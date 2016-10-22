940a0c0a0a6828632a0b52047a5529417c10209d
therealveggie <gv.veggie.321@gmail.com>
2016-10-22 12:18:02 -0400

    idk

---
+++ src/Character.java
@@ -0,0 +1,158 @@
+/**
+ * Class for each character
+ */
+
+//imports
+import java.io.*;
+import java.util.ArrayList;
+
+public class Character {
+  //globals
+  private String race, char_class, name, eye, hair, skin, height, weight, biography, alignment;
+  private int level, strength, dexterity, constitution, intelligence, wisdom, charisma;
+  private ArrayList[7] inventory;
+  FileInputStream in =null;
+  
+  //constructor
+  public Character(String char_class, String race, String name, String eye, String hair, String skin, String weight, String biography, String alignment) {
+    this.name = name;
+    this.char_class=char_class;
+    this.race= race;
+    this.eye = eye;
+    this.hair = hair;
+    this.skin = skin;
+    this.weight = weight;
+    this.biography = biography;
+    
+    level = 1;
+    strength = 8;
+    dexterity = 8;
+    constitution = 8;
+    intelligence = 8;
+    wisdom = 8;
+    charisma = 8;
+  }
+  //load
+  public Character(String filename)
+  {
+    String thisLine = null;
+    
+    try{
+      BufferedReader br = new BufferedReader(filename+".txt");
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+    
+    this.name=br.readLine();
+    this.char_class=br.readLine();
+    this.race = br.readLine();
+    this.level=Integer.parseInt(br.readLine());
+    this.strength= Integer.parseInt(br.readLine());
+    this.dexterity=Integer.parseInt(br.readLine());
+    this.constitution=Integer.parseInt(br.readLine());
+    this.intelligence=Integer.parseInt(br.readLine());
+    this.wisdom=Integer.parseInt(br.readLine());
+    this.charisma=Integer.parseInt(br.readLine());
+  }
+  
+  public void save(String filename){
+    File out =new File(filename+".txt");
+    out.createNewFile();
+    PrintWriter outFile = new PrintWriter(out);
+    outFile.println(this.name);
+    outFile.println(this.char_class);
+    outFile.println(this.race);
+    outFile.println(this.level);
+    outFile.println(this.strength);
+    outFile.println(this.dexterity);
+    outFile.println(this.constitution);
+    outFile.println(this.intelligence);
+    outFile.println(this.wisdom);
+    outFile.println(this.charisma);
+    outFile.close();
+    
+  }
+  
+  public String readFile(){
+    char c;
+    String ans;
+    while ((c = in.read()) != '\n'){
+      ans+=c;
+    }
+    return ans;
+  }
+  
+  //getters
+  public ArrayList get_inventory(){
+    return inventory;
+  }
+  public String get_class(){
+    return char_class;
+  }
+  public String get_name(){
+    return name;
+  }
+  public String get_race(){
+    return race;
+  }
+  public int get_level(){
+    return level;
+  }
+  public int get_strength(){
+    return level;
+  }
+  public int get_dexterity(){
+    return level;
+  }
+  public int get_constituion(){
+    return level;
+  }
+  public int get_intelligence(){
+    return level;
+  }
+  public int get_wisdom(){
+    return level;
+  }
+  public int get_charisma(){
+    return level;
+  }
+  
+  //Setters
+  public void set_inventory(ArrayList inventory){
+    this.inventory = inventory;
+  }
+  public void set_name(String name){
+    this.name= name;
+  }
+  public void set_class(String char_class){
+    this.char_class= char_class;
+  }
+  public void set_race(String race){
+    this.race= race;
+  }
+  public void set_level(int level){
+    this.level=level;
+  }
+  public void set_strength(int strength){
+    this.strength=strength;
+  }
+  public void set_dexterity(int dexterity){
+    this.dexterity=dexterity;
+  }
+  public void set_constitution(int constitution){
+    this.constitution=constitution;
+  }
+  public void set_intellgience(int intelligence){
+    this.intelligence=intelligence;
+  }
+  public void set_wisdom(int wisdom){
+    this.wisdom=wisdom;
+  }
+  public void set_charisma(int charisma){
+    this.charisma=charisma;
+  }
+  public void get_race(String race){
+    this.race=race;
+  }
+  
+  }
